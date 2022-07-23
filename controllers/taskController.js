cfde3db99b52bc9ca1d86f5dfd24635e8bea3cb5
mohiuddinmohi <mohiuddin0240@gmail.com>
2022-07-23 10:07:16 +0600

    refactor: task controller

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -3,9 +3,11 @@ const Task = require("../models/TaskModel");
 const getAllTasks = async (_, res) => {
   try {
     const tasks = await Task.find({});
-    res.status(200).json({ tasks });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+    res
+      .status(200)
+      .json({ status: "success", data: { tasks, amountOfTasks: tasks.length } });
+  } catch (err) {
+    res.status(500).json({ msg: err });
   }
 };
 
@@ -18,18 +20,18 @@ const getSingleTask = async (req, res) => {
       return res.status(404).json({ msg: `No work with id: ${taskID}` });
     }
 
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+    res.status(200).json({ status: "success", data: { task } });
+  } catch (err) {
+    res.status(500).json({ msg: err });
   }
 };
 
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
-    res.status(201).json(task);
-  } catch (error) {
-    res.status(500).json(error);
+    res.status(201).json({ status: "success", data: { task } });
+  } catch (err) {
+    res.status(500).json({ msg: err });
   }
 };
 
@@ -38,15 +40,15 @@ const updateTask = async (req, res) => {
     const { id: taskID } = req.params;
     const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
       new: true,
-      runValidators: true
+      runValidators: true,
     });
 
     if (!task) {
       return res.status(404).json({ msg: `No work with id: ${taskID}` });
     }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+    res.status(200).json({ status: "success", data: { task } });
+  } catch (err) {
+    res.status(500).json({ msg: err });
   }
 };
 
@@ -59,9 +61,9 @@ const deleteTask = async (req, res) => {
       return res.status(404).json({ msg: `No work with id: ${taskID}` });
     }
 
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+    res.status(200).json({ status: "success", data: { task } });
+  } catch (err) {
+    res.status(500).json({ msg: err });
   }
 };
 
